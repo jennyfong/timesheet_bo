9d654b2af3d888e1fa1b54a12b027bdd326f400e
Jenny Fong <askme@jennyfong.net>
2012-10-24 16:48:34 +0100

    added basic user functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
-    User.create(params)
+    User.create(params[:user])
+    redirect_to users_url
   end
 
   def edit
