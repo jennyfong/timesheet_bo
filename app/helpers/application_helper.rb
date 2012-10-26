1f06951fd6c150630f3803b42352f8eb559757ad
Jenny Fong <askme@jennyfong.net>
2012-10-26 19:29:43 +0100

    updated default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,22 @@ module ApplicationHelper
 
   end
 
+  def default_hour(time_log)
+    if time_log.start_time
+      time_log.start_time.strftime("%H")
+    else
+      "%02d"% Time.current.hour
+    end
+
+  end
+
+  def default_minute(time_log)
+    if time_log.start_time
+      time_log.start_time.strftime("%M")
+    else
+      "%02d"% Time.current.min
+    end
+
+  end
+
 end
