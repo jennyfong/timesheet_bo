16e469f57101cd1ce75d3984a4dce56c3d51ad2f
Jenny Fong <askme@jennyfong.net>
2012-10-29 08:00:23 +0000

    cleaned up codes to use rails helpers:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,5 @@
 module ApplicationHelper
 
-  def humanize_time(time_in_second=0)
-
-    Time.at(time_in_second).gmtime.strftime("%H hrs %M mins")
-
-
-  end
 
   def default_hour(input_time)
     if !input_time.blank?
--- app/helpers/time_logs_helper.rb
+++ app/helpers/time_logs_helper.rb
@@ -1,21 +1,5 @@
 module TimeLogsHelper
 
-  def hours_interval
-    list = []
 
-    for i in 0..24 do
-      list << "%02d" %i
-    end
-
-    list
-  end
-
-  def minute_interval
-    list = []
-    for i in 0..60 do
-      list << "%02d"%i
-    end
-    list
-  end
 
 end
